21a0c02da094e7abdac9b72ca676b98988c7a2ed
Artem Borodovoi <a.borodovoi@gmail.com>
2021-10-24 23:26:03 +0300

    start project and added services

---
+++ src/services/MarvelService.js
@@ -0,0 +1,26 @@
+class MarvelService {
+  _apiBase = "https://gateway.marvel.com:443/v1/public/";
+  _apiKey = "apikey=8e21b658adf2f1d4a9f2805377dc9f68";
+
+  getResource = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  getAllCharacters = () => {
+    return this.getResource(
+      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+    );
+  };
+
+  getCharacter = (id) => {
+    return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+  };
+}
+
+export default MarvelService;
