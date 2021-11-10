7771d0d42120a0b68e7e47dcb7acc2765039e18c
Artem Borodovoi <a.borodovoi@gmail.com>
2021-11-10 18:38:44 +0200

    selected item

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import PropTypes from "prop-types";
+
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import MarvelService from "../../services/MarvelService";
@@ -57,8 +58,22 @@ class CharList extends Component {
     });
   };
 
+  itemRefs = [];
+
+  setRef = (ref) => {
+    this.itemRefs.push(ref);
+  };
+
+  focusOnItem = (id) => {
+    this.itemRefs.forEach((item) =>
+      item.classList.remove("char__item_selected")
+    );
+    this.itemRefs[id].classList.add("char__item_selected");
+    this.itemRefs[id].focus();
+  };
+
   renderItems(arr) {
-    const items = arr.map((item) => {
+    const items = arr.map((item, i) => {
       let imgStyle = { objectFit: "cover" };
       if (
         item.thumbnail ===
@@ -70,15 +85,25 @@ class CharList extends Component {
       return (
         <li
           className="char__item"
+          tabIndex={0}
+          ref={this.setRef}
           key={item.id}
-          onClick={() => this.props.onCharSelected(item.id)}
+          onClick={() => {
+            this.props.onCharSelected(item.id);
+            this.focusOnItem(i);
+          }}
+          onKeyPress={(e) => {
+            if (e.key === " " || e.key === "Enter") {
+              this.props.onCharSelected(item.id);
+              this.focusOnItem(i);
+            }
+          }}
         >
           <img src={item.thumbnail} alt={item.name} style={imgStyle} />
           <div className="char__name">{item.name}</div>
         </li>
       );
     });
-
     return <ul className="char__grid">{items}</ul>;
   }
 
@@ -103,7 +128,7 @@ class CharList extends Component {
           style={{ display: charEnded ? "none" : "block" }}
           onClick={() => this.onRequest(offset)}
         >
-          <div className="inner">load more </div>
+          <div className="inner">load more</div>
         </button>
       </div>
     );
